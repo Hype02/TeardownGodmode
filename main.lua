2c5573755234b0e3a1cda27bb009e84c7f771cab
= <=>
2021-03-05 18:55:44 +0100

    Tiny optimalization as I got more into the game.

--- main.lua
+++ main.lua
@@ -1,14 +1,6 @@
-savedPlayerTransform = nil
-visible = 0
-cpMessage = "CHECKPOINT SAVED"
-
-
-function init()
-
-end
-
 
 function tick()
-	local hp = GetPlayerHealth()
-		SetPlayerHealth(10000)
+	if GetPlayerHealth() != 1 then
+		SetPlayerHealth(1)
+	end
 end

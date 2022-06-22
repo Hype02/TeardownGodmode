55de2d51cab381d41b04836fff3fdb0f3718b2e4
Hype02 <sebklawik@gmail.com>
2022-06-22 21:30:30 +0200

    Optimalization of main tick. See comment in code for more

--- main.lua
+++ main.lua
@@ -1,8 +1,36 @@
 health = 1
 
+lastTickTime = 0.0
+
+function init()
+
+lastTickTime = GetTime()
+
+end
+
 function tick()
+
+	
+
+	-- DebugPrint( GetTime() - lastTickTime)
+	-- 22.06.2022 Optimalization Patch | So now the health changes every 100ms not every possible tick
+	if(GetTime() - lastTickTime >= 0.1 ) then 
+		executeTickScript()
+		lastTickTime = GetTime()
+	end
+
+
+
+	
+
+
+end
+
+function executeTickScript()
+
 	health = GetPlayerHealth()
 	if health < 10 then
-		SetPlayerHealth(10)
+		SetPlayerHealth(10000000)
 	end
+
 end
